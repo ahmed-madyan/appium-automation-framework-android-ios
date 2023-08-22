298c13097b0d91de670027ee88c9288321f2aba1
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-08-23 01:26:44 +0300

    .

--- src/main/java/driver/BrowserStackInitializer.java
+++ src/main/java/driver/BrowserStackInitializer.java
@@ -13,6 +13,7 @@ import readers.properties_reader.PropertiesDataManager;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 
 public class BrowserStackInitializer {
@@ -25,11 +26,13 @@ public class BrowserStackInitializer {
     private static final DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
     private static final HashMap<String, Object> browserstackOptions = new HashMap<>();
     private static AppiumDriver appiumDriver;
+    private static String platformName;
 
     protected static AppiumDriver browserStackInitialization(String platformName) {
+        BrowserStackInitializer.platformName = platformName;
         System.out.println("TargetRemoteExecution: " + PropertiesConfigurations.getExecutionAddressConfig());
         switch (PropertiesConfigurations.getExecutionAddressConfig()) {
-            case "manual" -> setupManually(platformName);
+            case "manual" -> setupManually();
             case "yml" -> setupYML();
             default -> {
                 System.out.println("Kindly set the target remote execution option.");
@@ -39,7 +42,7 @@ public class BrowserStackInitializer {
         return appiumDriver;
     }
 
-    private static void setupManually(String platformName) {
+    private static void setupManually() {
         switch (platformName) {
             case "Android" -> initAndroidDriver();
             case "iOS" -> initIOSDriver();
@@ -92,34 +95,11 @@ public class BrowserStackInitializer {
         //Organize tests
         browserstackOptions.put("projectName", PropertiesDataManager.getProperty("androidProjectName", PropertiesDataManager.Capability.BROWSERSTACK));
         /**********************************************************************************************************/
-
         /****************************************Organize tests****************************************************/
         //Organize tests
         browserstackOptions.put("buildName", PropertiesDataManager.getProperty("androidBuildName", PropertiesDataManager.Capability.BROWSERSTACK));
-        browserstackOptions.put("buildIdentifier", new Timestamp(System.currentTimeMillis()).toString());
         /**********************************************************************************************************/
-        /****************************************Set debugging options*********************************************/
-        //Set debugging options
-        //1- Text logs are enabled by default, and cannot be disabled
-        //2- Network Logs are disabled by default. To enable network logs use its capability
-        browserstackOptions.put("networkLogs", PropertiesDataManager.getProperty("networkLogs", PropertiesDataManager.Capability.BROWSERSTACK));
-        //3- Device logs are enabled by default. To disable device logs use its capability
-        browserstackOptions.put("deviceLogs", PropertiesDataManager.getProperty("deviceLogs", PropertiesDataManager.Capability.BROWSERSTACK));
-        //4- Appium logs are enabled by default. To disable Appium logs use its capability
-        browserstackOptions.put("appiumLogs", PropertiesDataManager.getProperty("appiumLogs", PropertiesDataManager.Capability.BROWSERSTACK));
-        //5- Visual logs are disabled by default. To enable visual logs use its capability
-        browserstackOptions.put("debug", PropertiesDataManager.getProperty("debug", PropertiesDataManager.Capability.BROWSERSTACK));
-        //6- Video logs are enabled by default. Note that video recording slightly increases the text execution time. To disable video logs use its capability
-        browserstackOptions.put("video", PropertiesDataManager.getProperty("video", PropertiesDataManager.Capability.BROWSERSTACK));
-        /**********************************************************************************************************/
-        /****************************************Set Appium version************************************************/
-        //Set Appium version
-        browserstackOptions.put("appiumVersion", PropertiesDataManager.getProperty("appiumVersion", PropertiesDataManager.Capability.BROWSERSTACK));
-        /**********************************************************************************************************/
-        /*********************************Set browser stack capability options ************************************/
-        //Set bstack:options capabilities
-        desiredCapabilities.setCapability("bstack:options", browserstackOptions);
-        /**********************************************************************************************************/
+        setCommonDesiredCapabilities();
         //Initialize the driver and launch the app
         try {
             System.out.println("Android Desired Capabilities: " + desiredCapabilities);
@@ -153,7 +133,19 @@ public class BrowserStackInitializer {
         /****************************************Organize tests****************************************************/
         //Organize tests
         browserstackOptions.put("buildName", PropertiesDataManager.getProperty("iOSBuildName", PropertiesDataManager.Capability.BROWSERSTACK));
-        browserstackOptions.put("buildIdentifier", new Timestamp(System.currentTimeMillis()).toString());
+        /**********************************************************************************************************/
+        setCommonDesiredCapabilities();
+        //Initialize the driver and launch the app
+        try {
+            System.out.println("iOS Desired Capabilities: " + desiredCapabilities);
+            appiumDriver = new IOSDriver(new URL(browserStack_ServerURL), desiredCapabilities);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void setCommonDesiredCapabilities() {
+        browserstackOptions.put("buildIdentifier", new SimpleDateFormat("yyyy-MM-dd HH:mm").format(new Timestamp(System.currentTimeMillis())));
         /**********************************************************************************************************/
         /****************************************Set debugging options*********************************************/
         //Set debugging options
@@ -168,21 +160,31 @@ public class BrowserStackInitializer {
         browserstackOptions.put("debug", PropertiesDataManager.getProperty("debug", PropertiesDataManager.Capability.BROWSERSTACK));
         //6- Video logs are enabled by default. Note that video recording slightly increases the text execution time. To disable video logs use its capability
         browserstackOptions.put("video", PropertiesDataManager.getProperty("video", PropertiesDataManager.Capability.BROWSERSTACK));
+        /****************************************Set Interactive Debugging*****************************************/
+        browserstackOptions.put("interactiveDebugging", PropertiesDataManager.getProperty("interactiveDebugging", PropertiesDataManager.Capability.BROWSERSTACK));
+        /**********************************************************************************************************/
+        /****************************************Set Test Observability********************************************/
+//        browserstackOptions.put("testObservability", PropertiesDataManager.getProperty("testObservability", PropertiesDataManager.Capability.BROWSERSTACK));
         /**********************************************************************************************************/
         /****************************************Set Appium version************************************************/
         //Set Appium version
         browserstackOptions.put("appiumVersion", PropertiesDataManager.getProperty("appiumVersion", PropertiesDataManager.Capability.BROWSERSTACK));
+        /****************************************Set Device Settings************************************************/
+        //Set Device Settings
+        browserstackOptions.put("deviceOrientation", PropertiesDataManager.getProperty("deviceOrientation", PropertiesDataManager.Capability.BROWSERSTACK));
+        /**********************************************************************************************************/
+        /****************************************Set Security Settings*********************************************/
+        //Set Security Settings
+//        browserstackOptions.put("acceptInsecureCerts", PropertiesDataManager.getProperty("acceptInsecureCerts", PropertiesDataManager.Capability.BROWSERSTACK));
+//        browserstackOptions.put("acceptSslCert", PropertiesDataManager.getProperty("acceptSslCert", PropertiesDataManager.Capability.BROWSERSTACK));
+        /**********************************************************************************************************/
+        /****************************************Set Framework of test suite************************************************/
+        //Set Device Settings
+//        browserstackOptions.put("framework", PropertiesDataManager.getProperty("framework", PropertiesDataManager.Capability.BROWSERSTACK));
         /**********************************************************************************************************/
         /*********************************Set browser stack capability options ************************************/
         //Set bstack:options capabilities
         desiredCapabilities.setCapability("bstack:options", browserstackOptions);
         /**********************************************************************************************************/
-        //Initialize the driver and launch the app
-        try {
-            System.out.println("iOS Desired Capabilities: " + desiredCapabilities);
-            appiumDriver = new IOSDriver(new URL(browserStack_ServerURL), desiredCapabilities);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
